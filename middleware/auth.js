cf315268157e7f7babdc097b5330722714ed1908
zgq@qq.com <zgq@qq.com>
2026-05-08 15:54:47 +0800

    fix: 鉴权问题修改

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 function adminAuth(req, res, next) {
   const ADMIN_TOKEN = process.env.ADMIN_TOKEN || 'guyuan-admin-token'
-  const token = req.headers['x-admin-token']
+  const token = req.headers['x-admin-token'] || (req.query && req.query.token)
   if (!token || token !== ADMIN_TOKEN) {
     return res.json({ code: 403, message: '无操作权限' })
   }
